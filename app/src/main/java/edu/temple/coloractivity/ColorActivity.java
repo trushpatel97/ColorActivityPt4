056cc8a640b155a35d18d2fb7d5176b40a6b4f82
Trush Patel <tug63585@protonmail.com>
2020-02-15 14:32:04 -0500

    Got the spinner to show the colors and once selected, it changes that color to white. I used a spinner set on item selected listener to change the view of the item we selected from the spinner. The else-if conditions checks to see which element we selected then changes the view to the color to white indicating we selected this item

--- app/src/main/java/edu/temple/coloractivity/ColorActivity.java
+++ app/src/main/java/edu/temple/coloractivity/ColorActivity.java
@@ -1,17 +1,55 @@
 package edu.temple.coloractivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.media.session.PlaybackState;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+
 import java.util.ArrayList;
+
 
 public class ColorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        View view;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.myColors, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(new ColorAdapter(this));
+        view = this.getWindow().getDecorView();
+
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
+                    view.setBackgroundResource(R.color.white);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
     }
+
 }
--- app/src/main/java/edu/temple/coloractivity/ColorAdapter.java
+++ app/src/main/java/edu/temple/coloractivity/ColorAdapter.java
@@ -1,28 +1,61 @@
 package edu.temple.coloractivity;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ColorAdapter extends BaseAdapter {
+    ArrayList<Integer> colors;
+    Context context;
+
+    public ColorAdapter(Context context){
+
+        this.context=context;
+        colors = new ArrayList<Integer>();
+        int retrieve []=context.getResources().getIntArray(R.array.myColors);
+        for(int i:retrieve)
+        {
+            colors.add(i);
+        }
+    }
     @Override
     public int getCount() {
-        return 0;
+        return colors.size();
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public Object getItem(int args) {
+        return colors.get(args);
     }
 
     @Override
-    public long getItemId(int position) {
-        return 0;
+    public long getItemId(int args) {
+        return args;
+    }
+
+    public String getElementFromColors(int position){
+        String retrieve []=context.getResources().getStringArray(R.array.myColors);
+        return retrieve[position];
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        LayoutInflater inflater=LayoutInflater.from(context);
+        convertView=inflater.inflate(android.R.layout.simple_spinner_dropdown_item, null);
+        TextView txv=(TextView)convertView.findViewById(android.R.id.text1);
+        txv.setBackgroundColor(colors.get(position));
+        txv.setTextSize(20f);
+        txv.setTextColor(Color.parseColor("#FFFFFF"));
+        txv.setText(getElementFromColors(position));
+        return convertView;
     }
+
 }
