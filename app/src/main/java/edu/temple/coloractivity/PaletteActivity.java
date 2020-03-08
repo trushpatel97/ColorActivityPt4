c3b260b1767283647b999faeb98d2438051bafd3
Trush Patel <tug63585@protonmail.com>
2020-03-07 23:20:26 -0500

    Crashed when app changed language in the color activity, therefore deleted r.color.white which will disable the spinner color change

--- app/src/main/java/edu/temple/coloractivity/PaletteActivity.java
+++ app/src/main/java/edu/temple/coloractivity/PaletteActivity.java
@@ -1,123 +1,102 @@
 package edu.temple.coloractivity;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.media.session.PlaybackState;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
+import edu.temple.coloractivity.CanvasActivity;
+import edu.temple.coloractivity.ColorAdapter;
+import edu.temple.coloractivity.R;
+
 
 public class PaletteActivity extends AppCompatActivity {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String CurrentLang = Locale.getDefault().getLanguage();
-
-        setTitle("Palette Activity");
+        if(CurrentLang.equals("es")) {
+            setTitle("Actividad de paleta");
+        }else{
+            setTitle("Palette Activity ");
+        }
         final Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this, R.array.myColors, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(
                 android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<CharSequence> stringNames = ArrayAdapter.createFromResource(this, R.array.myStrings, android.R.layout.simple_spinner_item);
+        stringNames.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         if(CurrentLang.equals("es")){
-            ArrayAdapter<CharSequence> stringNames = ArrayAdapter.createFromResource(this,R.array.myStrings, android.R.layout.simple_spinner_item);
-            stringNames.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            ArrayAdapter<CharSequence> stringName = ArrayAdapter.createFromResource(this,R.array.myStrings, android.R.layout.simple_spinner_item);
+            stringName.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         }else{
-            ArrayAdapter<CharSequence> stringNames = ArrayAdapter.createFromResource(this,R.array.myStrings, android.R.layout.simple_spinner_item);
-            stringNames.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            ArrayAdapter<CharSequence> stringName = ArrayAdapter.createFromResource(this,R.array.myStrings, android.R.layout.simple_spinner_item);
+            stringName.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         }
 
 
         spinner.setAdapter(new ColorAdapter(this));
+        final boolean userSelected = false;
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
                 if(position == 0){
-                    System.out.println("Ignore this to make it shut up about no response");;
                 }
                 else if(position == 1){
-                    view.setBackgroundResource(R.color.white);
                     Intent i = new Intent(PaletteActivity.this, CanvasActivity.class);
                     i.putExtra("position",1);
                     PaletteActivity.this.startActivity(i);
                 }else if(position == 2){
-                    view.setBackgroundResource(R.color.white);
                     Intent i = new Intent(PaletteActivity.this, CanvasActivity.class);
                     i.putExtra("position",2);
                     PaletteActivity.this.startActivity(i);
-
                 }else if(position == 3){
-                    view.setBackgroundResource(R.color.white);
                     Intent i = new Intent(PaletteActivity.this, CanvasActivity.class);
                     i.putExtra("position",3);
                     PaletteActivity.this.startActivity(i);
-
                 }else if(position == 4){
-                    view.setBackgroundResource(R.color.white);
                     Intent i = new Intent(PaletteActivity.this, CanvasActivity.class);
                     i.putExtra("position",4);
                     PaletteActivity.this.startActivity(i);
-
                 }else if(position == 5){
-                    view.setBackgroundResource(R.color.white);
                     Intent i = new Intent(PaletteActivity.this, CanvasActivity.class);
                     i.putExtra("position",5);
                     PaletteActivity.this.startActivity(i);
-
                 }else if(position == 6){
-                    view.setBackgroundResource(R.color.white);
                     Intent i = new Intent(PaletteActivity.this, CanvasActivity.class);
                     i.putExtra("position",6);
                     PaletteActivity.this.startActivity(i);
-
                 }else if(position == 7){
-                    view.setBackgroundResource(R.color.white);
                     Intent i = new Intent(PaletteActivity.this, CanvasActivity.class);
                     i.putExtra("position",7);
                     PaletteActivity.this.startActivity(i);
-
                 }else if(position == 8){
-                    view.setBackgroundResource(R.color.white);
                     Intent i = new Intent(PaletteActivity.this, CanvasActivity.class);
                     i.putExtra("position",8);
                     PaletteActivity.this.startActivity(i);
-
                 }else if(position == 9){
-                    view.setBackgroundResource(R.color.white);
                     Intent i = new Intent(PaletteActivity.this, CanvasActivity.class);
                     i.putExtra("position",9);
                     PaletteActivity.this.startActivity(i);
                 }else if(position == 10){
-                    view.setBackgroundResource(R.color.white);
                     Intent i = new Intent(PaletteActivity.this, CanvasActivity.class);
                     i.putExtra("position",10);
                     PaletteActivity.this.startActivity(i);
                 }
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
     }
-
 }
